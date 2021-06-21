b663f973bfdea086c91ed62fa154282078a17565
Jitesh Bagul <78952376+jitesh-cloud@users.noreply.github.com>
2021-06-21 23:04:31 +0530

    Update 3. BASIC-SELECT.sql

--- Easy/3. BASIC-SELECT.sql
+++ Easy/3. BASIC-SELECT.sql
@@ -3,3 +3,12 @@ SELECT * FROM CITY WHERE ID = 1661;
 
 --> 2
 SELECT * FROM CITY WHERE COUNTRYCODE = 'JPN';
+
+--> 3
+SELECT NAME FROM CITY WHERE COUNTRYCODE = 'JPN';
+
+--> 4 Query a list of CITY and STATE from the STATION table.
+SELECT CITY,STATE FROM STATION;
+
+--> 5 Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.
+SELECT DISTINCT CITY FROM STATION WHERE ID % 2 = 0;
