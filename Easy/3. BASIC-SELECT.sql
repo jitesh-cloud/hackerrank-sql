a6c856a90068c07524094e23e7d96304d72c0876
Jitesh Bagul <78952376+jitesh-cloud@users.noreply.github.com>
2021-06-21 23:21:45 +0530

    Update 3. BASIC-SELECT.sql

--- Easy/3. BASIC-SELECT.sql
+++ Easy/3. BASIC-SELECT.sql
@@ -12,3 +12,6 @@ SELECT CITY,STATE FROM STATION;
 
 --> 5 Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.
 SELECT DISTINCT CITY FROM STATION WHERE ID % 2 = 0;
+
+--> 6 Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
+SELECT COUNT(CITY) - COUNT(DISTINCT CITY) FROM STATION;
