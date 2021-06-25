45156724d0637776c4a80da36e1608a96f59d425
Jitesh Bagul <78952376+jitesh-cloud@users.noreply.github.com>
2021-06-25 22:21:11 +0530

    Update 4. BASIC-SELECT-II.sql

--- Easy/4. BASIC-SELECT-II.sql
+++ Easy/4. BASIC-SELECT-II.sql
@@ -2,6 +2,11 @@
 as well as their respective lengths (i.e.: number of characters in the name). 
 If there is more than one smallest or largest city, 
 choose the one that comes first when ordered alphabetically.*/
-
 SELECT CITY,LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY), CITY LIMIT 1;
 SELECT CITY,LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY) DESC, CITY LIMIT 1;
+
+
+/* Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. 
+Your result cannot contain duplicates.*/
+SELECT CITY FROM STATION
+WHERE CITY LIKE 'A%' OR CITY LIKE 'E%' OR CITY LIKE 'I%' OR CITY LIKE 'O%' OR CITY LIKE 'U%';
