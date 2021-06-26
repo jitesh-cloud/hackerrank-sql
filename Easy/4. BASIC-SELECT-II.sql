081a7f502b12bca9a8bc56af933dfe570ef69c44
Jitesh Bagul <78952376+jitesh-cloud@users.noreply.github.com>
2021-06-27 00:19:46 +0530

    Create 4. BASIC-SELECT-II.sql

--- Easy/4. BASIC-SELECT-II.sql
+++ Easy/4. BASIC-SELECT-II.sql
@@ -8,5 +8,30 @@ SELECT CITY,LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY) DESC, CITY LIMIT 1;
 
 /* Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. 
 Your result cannot contain duplicates.*/
-SELECT CITY FROM STATION
+SELECT DISTINCT CITY FROM STATION
 WHERE CITY LIKE 'A%' OR CITY LIKE 'E%' OR CITY LIKE 'I%' OR CITY LIKE 'O%' OR CITY LIKE 'U%';
+
+
+/*Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. 
+Your result cannot contain duplicates.*/
+select distinct city from station
+where city like '%a' or city like '%e' or city like '%i' or city like '%o' or city like '%u';
+
+/*Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. 
+Your result cannot contain duplicates.*/
+SELECT DISTINCT CITY FROM STATION
+WHERE (CITY LIKE 'A%' OR CITY LIKE 'E%' OR CITY LIKE 'I%' OR CITY LIKE 'O%' OR CITY LIKE 'U%') AND (CITY LIKE '%A' OR CITY LIKE '%E' OR CITY LIKE '%I' OR CITY LIKE '%O' OR CITY LIKE '%U');
+
+/*Query the list of CITY names from STATION that do not start with vowels. 
+Your result cannot contain duplicates.*/
+SELECT DISTINCT CITY FROM STATION
+WHERE CITY REGEXP '^[^aeiou]';
+
+/*Query the list of CITY names from STATION that do not end with vowels. 
+Your result cannot contain duplicates.*/
+select distinct city from station
+where city not like '%a'
+and city not like '%e'
+and city not like '%o'
+and city not like '%i'
+and city not like '%u';
